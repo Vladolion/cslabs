8eef05a71109dd2418878a0552536f386b45847e
MarianaD167 <vladforschool@gmail.com>
2025-04-02 10:35:57 +0300

    lab2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,28 @@ class Program{
 
 		foreach (User user in users){
 			user.DisplayInfo();
+            Console.WriteLine("---------------");
+        }
+
+		foreach (User user in users)
+		{
+			if (user is Admin) {
+				(user as Admin).BlockUser(users[2]);
+			}
+			if (user is Moderator)
+			{
+				(user as Moderator).ModerateContent();
+			}
+			if (user is RegularUser) {
+				(user as RegularUser).PostComment();
+			}
+		}
+
+		List<string> passwords = new List<string> { "usseewa", "NAI", "abracadabra" };
+
+        Console.WriteLine("---------------");
+        for (int i = 0; i < users.Count; i++) {
+            Console.WriteLine(users[i].UserName + ": "+users[i].Authenticate(passwords[i]));
 		}
 	}
 }
